cc86adf589e1a7282c10a4a5c7a4dae70f3d06a5
cykhung <45437973+cykhung@users.noreply.github.com>
2021-05-16 11:19:36 -0400

    Initial commit of sumatrapdf.m.

---
+++ sumatrapdf.m
@@ -0,0 +1,32 @@
+function sumatrapdf(filenames)
+
+%%
+%       SYNTAX: sumatrapdf(filenames);
+%
+%  DESCRIPTION: Use SumatraPDF to open PDF files.
+%
+%        INPUT: - filenames (char or N-D cell array of char or 
+%                                    N-D array of categorical)
+%                   Filename(s).
+%
+%       OUTPUT: none.
+
+
+%% Force filenames into cell array.
+filenames = convert_filenames(filenames);
+
+
+%% Open PDF files.
+exe = fullfile(fileparts(mfilename('fullpath')), ...
+    'SumatraPDF-3.1.2-64', 'SumatraPDF.exe');
+for n = 1:numel(filenames)
+    cmd = [exe, ' ',                            ...
+           sprintf('"%s" ', filenames{n}),      ...
+           sprintf('&'),                        ...        
+        ];
+    dos(cmd);
+end
+
+
+end
+
